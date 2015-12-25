e84334d028f1b00914a256001a2a668a494a914c
RetroSoftware <bornemann-philipp@outlook.de>
2015-12-25 22:05:59 +0100

    Seiten

---
+++ files/lib/page/LTBPage.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace wcf\page;
+use wcf\system\WCF;
+
+/**
+ * @author	Philipp Bornemann
+ * @copyright	2016 LTB-Zone
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class LTBPage extends AbstractPage {
+	/**
+	 * @see	\wcf\page\AbstractPage::$templateName
+	 */
+	public $templateName = 'ltb';
+}
---
+++ files/lib/page/OtherLTBPage.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace wcf\page;
+use wcf\system\WCF;
+
+/**
+ * @author	Philipp Bornemann
+ * @copyright	2016 LTB-Zone
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class OtherLTBPage extends AbstractPage {
+	/**
+	 * @see	\wcf\page\AbstractPage::$templateName
+	 */
+	public $templateName = 'otherltb';
+}
